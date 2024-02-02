40d8da0252b720011498b6c9a1e255f04787bcf7
Ryabtsev <ryabts777@yandex.ru>
2024-02-02 23:51:24 +0300

    Полный поиск

---
+++ scaling_function.py
@@ -0,0 +1,28 @@
+import pprint
+
+import requests
+
+
+def spn_selection(geocode):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": geocode,
+        "format": "json"}
+
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+
+    if not response:
+        # обработка ошибочной ситуации
+        pass
+
+    # Преобразуем ответ в json-объект
+    json_response = response.json()
+    # Получаем первый топоним из ответа геокодера.
+    toponym = json_response["response"]["GeoObjectCollection"][
+        "featureMember"][0]["GeoObject"]
+    fi = float(toponym["boundedBy"]['Envelope']['upperCorner'].split()[0]) - float(toponym["boundedBy"]['Envelope']['lowerCorner'].split()[0])
+    se = float(toponym["boundedBy"]['Envelope']['upperCorner'].split()[1]) - float(
+        toponym["boundedBy"]['Envelope']['lowerCorner'].split()[1])
+    return str(fi), str(se)
