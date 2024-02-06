f77e7da2a864027d2eb653640dcd24f235d3b624
Ryabtsev <ryabts777@yandex.ru>
2024-02-06 23:08:55 +0300

    Программа работает, объясните что не так

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import scaling_function
 import sys
 from io import BytesIO
-# Этот класс поможет нам сделать картинку из потока байт
-
 import requests
 from PIL import Image
 
-# Пусть наше приложение предполагает запуск:
-# python search.py Москва, ул. Ак. Королева, 12
-# Тогда запрос к геокодеру формируется следующим образом:
+
 toponym_to_find = " ".join(sys.argv[1:])
 
 geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
@@ -24,19 +20,14 @@ if not response:
     # обработка ошибочной ситуации
     pass
 
-# Преобразуем ответ в json-объект
 json_response = response.json()
-# Получаем первый топоним из ответа геокодера.
 toponym = json_response["response"]["GeoObjectCollection"][
     "featureMember"][0]["GeoObject"]
-# Координаты центра топонима:
 toponym_coodrinates = toponym["Point"]["pos"]
-# Долгота и широта:
 toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
 
 spn = scaling_function.spn_selection(toponym_to_find)
 
-# Собираем параметры для запроса к StaticMapsAPI:
 map_params = {
     "ll": ",".join([toponym_longitude, toponym_lattitude]),
     "spn": ",".join(spn),
@@ -45,10 +36,7 @@ map_params = {
 }
 
 map_api_server = "http://static-maps.yandex.ru/1.x/"
-# ... и выполняем запрос
 response = requests.get(map_api_server, params=map_params)
 
 Image.open(BytesIO(
     response.content)).show()
-# Создадим картинку
-# и тут же ее покажем встроенным просмотрщиком операционной системы
