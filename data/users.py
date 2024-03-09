13a4313c72befbce7f32bd44bef2d835cf5d7ed2
Ivan_Ryabtsev <ryabts777@yandex.ru>
2024-03-09 15:40:38 +0300

    журнал работ

---
+++ data/users.py
@@ -0,0 +1,25 @@
+import sqlalchemy
+import datetime as dt
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    position = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    speciality = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, nullable=True, unique=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=dt.datetime.now)
+
+    jobs = orm.relationship("Jobs", back_populates='user')
+    department = orm.relationship("Department", back_populates='user')
+
+    def __repr__(self):
+        return f'<Colonist> {self.id} {self.surname} {self.name}'
--- main.py
+++ main.py
@@ -1,41 +1,27 @@
-import scaling_function
-import sys
-from io import BytesIO
-import requests
-from PIL import Image
+from flask import Flask, render_template
+from data import db_session
+from data.users import User
+from data.jobs import Jobs
+from data.departments import Department
+import datetime as dt
 
-toponym_to_find = " ".join(sys.argv[1:])
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '1234567890'
 
-geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
 
-geocoder_params = {
-    "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-    "geocode": toponym_to_find,
-    "format": "json"}
+@app.route('/')
+@app.route('/index')
+def index():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    print(jobs)
+    return render_template("index.html", jobs=jobs)
 
-response = requests.get(geocoder_api_server, params=geocoder_params)
 
-if not response:
-    # обработка ошибочной ситуации
-    pass
+def main():
+    db_session.global_init('db/mars_explorer.db')
+    app.run()
 
-json_response = response.json()
-toponym = json_response["response"]["GeoObjectCollection"][
-    "featureMember"][0]["GeoObject"]
-toponym_coodrinates = toponym["Point"]["pos"]
-toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
 
-spn = scaling_function.spn_selection(toponym_to_find)
-
-map_params = {
-    "ll": ",".join([toponym_longitude, toponym_lattitude]),
-    "spn": ",".join(spn),
-    'pt': ",".join([toponym_longitude, toponym_lattitude]) + ',vkbkm',
-    "l": "map"
-}
-
-map_api_server = "http://static-maps.yandex.ru/1.x/"
-response = requests.get(map_api_server, params=map_params)
-
-Image.open(BytesIO(
-    response.content)).show()
+if __name__ == '__main__':
+    main()
